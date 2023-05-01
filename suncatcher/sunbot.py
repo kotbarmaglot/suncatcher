2e31a7d63b3ca3af61ac0af8d1138fc8ac945fa3
kotbarmaglot <allonewage@gmail.com>
2023-05-01 08:40:47 +0300

    beta-v with delete messages

--- suncatcher/sunbot.py
+++ suncatcher/sunbot.py
@@ -1,195 +1,312 @@
 from .config import API_KEY
-from .vksunbot import *
+from .vksunbot import get_moon, get_moon_amount
 import telebot
 import json
 from telebot import types
-from telebot.types import InputMediaPhoto
+from pathlib import Path
 
 
 bot = telebot.TeleBot(API_KEY)
 
-with open("info_product.json", "r") as read_file:
-    data = json.load(read_file)
+
+def save_message_chat_id(message):
+    user_id = message.chat.id
+    user_patch = Path(f"suncatcher/user_file/{user_id}.json")
+
+    print('save message', message)
+
+    with open(user_patch, "r") as read_file:
+        user_data = json.load(read_file)
+
+    user_data['id_mes'] = message.message_id
+    user_data['id_chat'] = message.chat.id
+
+    with open(user_patch, "w") as write_file:
+        json.dump(user_data, write_file)
 
 
-def fill_big_lunnica(message):
-    description, title, url_photo, id_product = get_big_lunnica(data, message)
-    print(description, title, url_photo)
+def get_message_chat_id(message):
+    user_id = message.from_user.id
+    user_patch = Path(f"suncatcher/user_file/{user_id}.json")
+
+    with open(user_patch, "r") as read_file:
+        user_data = json.load(read_file)
+
+    return user_data['id_mes'], user_data['id_chat']
+
+
+def set_current_size_sun(message, size):
+    user_id = message.from_user.id
+    user_patch = Path(f"suncatcher/user_file/{user_id}.json")
+
+    with open(user_patch, "r") as read_file:
+        user_data = json.load(read_file)
+        user_data['current_size'] = size
+
+    with open(user_patch, "w") as write_file:
+        json.dump(user_data, write_file)
+
+
+def set_current_number_sun(message, current, size):
+    user_id = message.from_user.id
+    user_patch = Path(f"suncatcher/user_file/{user_id}.json")
+
+    with open(user_patch, "r") as read_file:
+        user_data = json.load(read_file)
+
+    user_data['catalog'][size]['current'] = current
+
+    with open(user_patch, "w") as write_file:
+        json.dump(user_data, write_file)
+
+
+def get_current_number_size_sun(message):
+    user_id = message.from_user.id
+    user_patch = Path(f"suncatcher/user_file/{user_id}.json")
+
+    with open(user_patch, "r") as read_file:
+        user_data = json.load(read_file)
+
+    current_size = user_data['current_size']
+    current_number = user_data['catalog'][current_size]['current']
+    all_size = user_data['catalog'][current_size]['all']
+
+    return current_number, current_size, all_size
+
+
+def create_or_get_user_file(message):
+    user_id = message.from_user.id
+    user_patch = Path(f"suncatcher/user_file/{user_id}.json")
+    amount = get_moon_amount()
+
+    user_data = {}
+    user_data['id'] = user_id
+    user_data['catalog'] = {}
+    user_data['catalog']['big'] = {}
+    user_data['catalog']['mid'] = {}
+    user_data['catalog']['low'] = {}
+    user_data['catalog']['big']['all'] = amount['big']
+    user_data['catalog']['mid']['all'] = amount['mid']
+    user_data['catalog']['low']['all'] = amount['low']
+    user_data['catalog']['big']['current'] = 0
+    user_data['catalog']['mid']['current'] = 0
+    user_data['catalog']['low']['current'] = 0
+    user_data['start'] = 1
+    user_data['current_size'] = ''
+    user_data["id_mes"] = 0
+    user_data['id_chat'] = 0
+
+    with open(user_patch, "w") as write_file:
+        json.dump(user_data, write_file)
+
+    return user_data
+
+
+def show_catalog(message):
+    title = 'Какие ловцы солнца вы хотите посмотреть:\n\n'
+    big_sun = '1. Ловцы общей длиной 40 см и больше, стоимостью 4000р.\n\n'
+    mid_sun = '2. Ловцы длиной ~ 30 см, стоимостью 3000р.\n\n'
+    low_sun = '3. Ловцы длиной ~ 20 см, стоимостью 2000р.\n\n'
+
+    text = f"{title}{big_sun}{mid_sun}{low_sun}"
 
     markup_inline = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton("Большие ловцы", callback_data='big')
+    item2 = types.InlineKeyboardButton("Средние ловцы", callback_data='mid')
+    item3 = types.InlineKeyboardButton("Малые ловцы", callback_data='low')
 
-    item = types.InlineKeyboardButton(text = '🎁 Выбрать ✍️ Написать мастеру', url = 'https://t.me/Lunar_room')
-    item2 = types.InlineKeyboardButton(text = '⭐️ В избранное', callback_data = 'big_lunnicy')
+    url_photo = 'https://sun9-42.userapi.com/impg/V7H81niHXYdgP2M_ZqbL4lRvuwGQGajdTvkdYw/KqT-wWKWTHw.jpg?size=2560x2560&quality=95&sign=79eab711010666d1651f6001fdf322d9&type=album'
 
-    markup_inline.add(item)
-    markup_inline.add(item2)
-
-    pic1 = 'https://sun6-23.userapi.com/impg/pRg4gqwTFhHSqBb5Do8_CbAh6gDl3gz86DC_Qw/UmSErGTB0f8.jpg?size=1080x1079&quality=95&sign=12ff1fd6996900453e878b3db40cf1b8&type=album'
-
-    pic2 = 'https://sun9-78.userapi.com/impg/_Y9m20t7FWYNtloSaiJi1yb0X8_Tcn47rljT9Q/80WHFlMFpdk.jpg?size=2560x2560&quality=95&sign=3f8255c76872459ae10bce313d0abddc&type=album'
-
-    media = [InputMediaPhoto(pic1, caption="test"), InputMediaPhoto(pic2)]
-    bot.send_media_group(message.from_user.id, media)
-
-    # bot.send_photo(message.from_user.id, url_photo[0], caption=description[0], reply_markup=markup_inline)
-
-
-def big_suncatcher(message):
-    description = "Большие ловцы солнца – общая длина с фурнитурой ~ 40 см, а размер цветочной основы 20 см."
-    url_photo = "https://sun9-42.userapi.com/impg/V7H81niHXYdgP2M_ZqbL4lRvuwGQGajdTvkdYw/KqT-wWKWTHw.jpg?size=2560x2560&quality=95&sign=79eab711010666d1651f6001fdf322d9&type=album"
-
-    markup_inline = types.InlineKeyboardMarkup()
-
-    item = types.InlineKeyboardButton(text = 'Луны', callback_data = 'big_moon')
-    item2 = types.InlineKeyboardButton(text = 'Лунницы', callback_data = 'big_lunnicy')
-    item3 = types.InlineKeyboardButton(text = 'Фазы луны', callback_data = 'big_fazy')
-    item4 = types.InlineKeyboardButton(text = 'Круги', callback_data = 'big_circle')
-    item5 = types.InlineKeyboardButton(text = 'Сердца', callback_data = 'big_hearts')
-    item6 = types.InlineKeyboardButton(text = 'Звёзды', callback_data = 'big_stars')
-
-    markup_inline.add(item)
+    markup_inline.add(item1)
     markup_inline.add(item2)
     markup_inline.add(item3)
-    markup_inline.add(item4)
-    markup_inline.add(item5)
-    markup_inline.add(item6)
 
-    bot.send_photo(message.from_user.id, url_photo, caption=description, reply_markup=markup_inline)
+    msg = bot.send_photo(message.from_user.id, url_photo, caption=text, reply_markup=markup_inline)
+
+    save_message_chat_id(msg)
+
+    print(msg)
+
+
+def show_choose_size_suncatcher(message, number_sun, size):
+
+    big = get_moon(size)
+
+    current_number, type, all_sun = get_current_number_size_sun(message)
+
+    print(big)
+    print(current_number)
+
+    url_photo = big[size][number_sun]['url_photo'][0]
+    count = f'{size}_count'
+    current_sun_text = f'{current_number+1}/{big[count]}'
+    description = big[size][number_sun]['description'] + current_sun_text
+
+    markup_inline = types.InlineKeyboardMarkup()
+
+    if current_number+1 == big[count]:
+        item = types.InlineKeyboardButton(text='Описание/Фото', callback_data='more')
+        item2 = types.InlineKeyboardButton(text='⬅️', callback_data='back')
+        item3 = types.InlineKeyboardButton(text='⛔️', callback_data='none')
+        item4 = types.InlineKeyboardButton(text='Каталог', callback_data='catalog')
+        item5 = types.InlineKeyboardButton(text=f'{current_sun_text}', callback_data='none')
+
+        markup_inline.add(item2, item5, item3)
+        markup_inline.add(item)
+        markup_inline.add(item4)
+
+    if current_number+1 == 1:
+        item = types.InlineKeyboardButton(text='Описание/Фото', callback_data='more')
+        item2 = types.InlineKeyboardButton(text='⛔️', callback_data='none')
+        item3 = types.InlineKeyboardButton(text='➡️', callback_data='next')
+        item4 = types.InlineKeyboardButton(text='Каталог', callback_data='catalog')
+        item5 = types.InlineKeyboardButton(text=f'{current_sun_text}', callback_data='none')
+
+        markup_inline.add(item2, item5, item3)
+        markup_inline.add(item)
+        markup_inline.add(item4)
+
+    if 1 < current_number+1 < big[count]:
+        item = types.InlineKeyboardButton(text='Описание/Фото', callback_data='more')
+        item2 = types.InlineKeyboardButton(text='⬅️', callback_data='back')
+        item3 = types.InlineKeyboardButton(text='➡️', callback_data='next')
+        item4 = types.InlineKeyboardButton(text='Каталог', callback_data='catalog')
+        item5 = types.InlineKeyboardButton(text=f'{current_sun_text}', callback_data='none')
+
+        markup_inline.add(item2, item5, item3)
+        markup_inline.add(item)
+        markup_inline.add(item4)
+
+    ms, ch = get_message_chat_id(message)
+
+    bot.delete_message(message.from_user.id, ms)
+    
+
+    msg = bot.send_photo(message.from_user.id, url_photo,  reply_markup=markup_inline)
+
+    save_message_chat_id(msg)
+
+
+def show_choose_size_suncatcher_more(message, number_sun, size):
+    
+    big = get_moon(size)
+
+    current_number, type, all_sun = get_current_number_size_sun(message)
+
+    print(big)
+    print(current_number)
+
+    url_photo = big[size][number_sun]['url_photo'][0]
+    count = f'{size}_count'
+    current_sun_text = f'{current_number+1}/{big[count]}'
+    description = big[size][number_sun]['description'] + current_sun_text
+
+    markup_inline = types.InlineKeyboardMarkup()
+
+    item = types.InlineKeyboardButton(text='Заказать', url='https://t.me/Lunar_room')
+    item4 = types.InlineKeyboardButton(text='Каталог', callback_data='catalog')
+
+    markup_inline.add(item)
+    markup_inline.add(item4)
+
+    ms, ch = get_message_chat_id(message)
+
+    bot.delete_message(message.from_user.id, ms)
+    
+    msg = bot.send_photo(message.from_user.id, url_photo, caption=description, reply_markup=markup_inline)
+
+    save_message_chat_id(msg)
 
 
 def mid_suncatcher(message):
-    description = "Средние ловцы солнца – общая длина с фурнитурой ~ 30 см, а размер цветочной основы 15 см."
-    url_photo = "https://sun9-42.userapi.com/impg/V7H81niHXYdgP2M_ZqbL4lRvuwGQGajdTvkdYw/KqT-wWKWTHw.jpg?size=2560x2560&quality=95&sign=79eab711010666d1651f6001fdf322d9&type=album"
-
-    markup_inline = types.InlineKeyboardMarkup()
-
-    item = types.InlineKeyboardButton(text = 'Луны', callback_data = 'mid_moon')
-    item2 = types.InlineKeyboardButton(text = 'Лунницы', callback_data = 'mid_lunnicy')
-    item3 = types.InlineKeyboardButton(text = 'Круги', callback_data = 'mid_circle')
-    item4 = types.InlineKeyboardButton(text = 'Сердца', callback_data = 'mid_hearts')
-    item5 = types.InlineKeyboardButton(text = 'Звёзды', callback_data = 'mid_stars')
-
-    markup_inline.add(item)
-    markup_inline.add(item2)
-    markup_inline.add(item3)
-    markup_inline.add(item4)
-    markup_inline.add(item5)
-
-    bot.send_photo(message.from_user.id, url_photo, caption=description, reply_markup=markup_inline)
+    pass
 
 
 def low_suncatcher(message):
-    description = "Ловцы солнца малые – общая длина с фурнитурой ~ 20 см, а размер цветочной основы 10 см."
-    url_photo = "https://sun9-42.userapi.com/impg/V7H81niHXYdgP2M_ZqbL4lRvuwGQGajdTvkdYw/KqT-wWKWTHw.jpg?size=2560x2560&quality=95&sign=79eab711010666d1651f6001fdf322d9&type=album"
-
-    markup_inline = types.InlineKeyboardMarkup()
-
-    item = types.InlineKeyboardButton(text = 'Луны', callback_data = 'low_moon')
-    item2 = types.InlineKeyboardButton(text = 'Лунницы', callback_data = 'low_lunnicy')
-    item3 = types.InlineKeyboardButton(text = 'Круги', callback_data = 'low_circle')
-    item4 = types.InlineKeyboardButton(text = 'Сердца', callback_data = 'low_hearts')
-    item5 = types.InlineKeyboardButton(text = 'Звёзды', callback_data = 'low_stars')
-
-    markup_inline.add(item)
-    markup_inline.add(item2)
-    markup_inline.add(item3)
-    markup_inline.add(item4)
-    markup_inline.add(item5)
-
-    bot.send_photo(message.from_user.id, url_photo, caption=description, reply_markup=markup_inline)
+    pass
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    print('start', message)
+
+    create_or_get_user_file(message)
+
     user_name = message.from_user.first_name
-    text_message = "Привет, " + f"{message.from_user.first_name}!" + "\n\nМеня зовут Оксана. Я создательница ловцов солнца. Хочешь я покажу тебе каталог товаров?"
+    text_message = "Привет, " + f"{user_name}!" + "\n\nМеня зовут Оксана. Я создательница ловцов солнца. Хочешь я покажу тебе каталог товаров?"
     url_photo = "https://sun9-54.userapi.com/impg/vnK5xGTaFNIB8DjusXniXDLUweZ8mp9O9H398g/d9_He-KNepo.jpg?size=1440x2160&quality=95&sign=ead03b10d6230c7a345507663da80d7f&type=album"
     markup_inline = types.InlineKeyboardMarkup()
-    item1 = types.InlineKeyboardButton(text = 'Посмотреть каталог товаров', callback_data = 'Каталог')
-    item2 = types.InlineKeyboardButton(text = 'Написать мне', url = 'https://t.me/Lunar_room')
+    item1 = types.InlineKeyboardButton(text='Каталог', callback_data='catalog')
     markup_inline.add(item1)
-    markup_inline.add(item2)
-    bot.send_photo(message.from_user.id, url_photo, caption=text_message, reply_markup=markup_inline)
+    msg = bot.send_photo(message.from_user.id, url_photo, caption=text_message, reply_markup=markup_inline)
+
+    save_message_chat_id(msg)
 
 
 @bot.callback_query_handler(func=lambda m: True)
 def callback_choice(message):
     t = message.data
-    print(t)
 
-    if t == 'Каталог':
-        big_sun = "Большие ловцы - размер 35см. Стоимость 4т.р.\n\n"
-        mid_sun = "Средние ловцы - размер 35см. Стоимость 4т.р.\n\n"
-        low_sun = 'Маленькие ловцы - размер 35см. Стоимость 4т.р.\n\n'
-        text = "Выбери свой ловец солнца \n\n" + f"{big_sun}{mid_sun}{low_sun}"
+    if t == 'catalog':
+        ms, ch = get_message_chat_id(message)
+        bot.delete_message(message.from_user.id, ms)
 
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("Большие ловцы")
-        item2=types.KeyboardButton("Средние ловцы")
-        item3=types.KeyboardButton("Малые ловцы")
-        item4=types.KeyboardButton("Акции")
-        # item5=types.KeyboardButton("Показать всё наличие")
+        set_current_size_sun(message, size='')
+        show_catalog(message)
+        set_current_number_sun(message, current=0, size='big')
+        set_current_number_sun(message, current=0, size='mid')
+        set_current_number_sun(message, current=0, size='low')
 
-        markup.add(item1, item2)
-        markup.add(item3, item4)
-        # markup.add(item5)
+    if t == 'big':
+        set_current_size_sun(message, t)
+        show_choose_size_suncatcher(message, number_sun=0, size=t)
 
-        msg = bot.send_message(message.from_user.id, text, reply_markup=markup)
-    
-    if t == 'big_moon':
-        x = get_big_moon(data, message)
-        print(x)
-        # print('big_moon')
+    if t == 'mid':
+        set_current_size_sun(message, t)
+        show_choose_size_suncatcher(message, number_sun=0, size=t)
 
-    if t == 'mid_moon':
+    if t == 'low':
+        set_current_size_sun(message, t)
+        show_choose_size_suncatcher(message, number_sun=0, size=t)
 
+    if t == 'next':
+        number, size, all = get_current_number_size_sun(message)
+        print(number)
+        set_current_number_sun(message, current=number+1, size=size)
+        show_choose_size_suncatcher(message, number_sun=number+1, size=size)
+
+    if t == 'back':
+        number, size, all = get_current_number_size_sun(message)
+        print(number)
+        set_current_number_sun(message, current=number-1, size=size)
+        show_choose_size_suncatcher(message, number_sun=number-1, size=size)
+
+    if t == 'more':
+        number, size, all = get_current_number_size_sun(message)
+        show_choose_size_suncatcher_more(message, number_sun=number, size=size)
+
+
+# @bot.message_handler(func=lambda m: True)
+# def handler(message):
+
+#     t = message.text
+
+#     if t == 'Каталог':
+#         set_current_size_sun(message, size='')
+#         show_catalog(message)
+#         set_current_number_sun(message, current=0, size='big')
+#         set_current_number_sun(message, current=0, size='mid')
+#         set_current_number_sun(message, current=0, size='low')
         
-        print('mid_moon')
-    
-    if t == 'low_moon':
-        print('low_moon')
-
-    if t == 'big_lunnicy':
-        fill_big_lunnica(message)
-
-    if t == 'mid_lunnicy':
-        print('mid_lunnicy')
-    
-    if t == 'low_lunnicy':
-        print('low_lunnicy')
-
-    if t == 'big_fazy':
-        print('big_fazy')
-
-    if t == 'mid_fazy':
-        print('mid_fazy')
-    
-    if t == 'low_fazy':
-        print('low_fazy')
 
 
-@bot.message_handler(func=lambda m: True)
-def main(message):
-    t = message.text
+#     if t == "Средние ловцы":
+#         mid_suncatcher(message)
 
-    if t == 'Большие ловцы':
-        big_suncatcher(message)
+#     if t == "Малые ловцы":
+#         low_suncatcher(message)
 
-    if t == "Средние ловцы":
-        mid_suncatcher(message)
-
-    if t == "Малые ловцы":
-        low_suncatcher(message)
-
-    if t == "Акции":
-        pass
-
-    if t == "Показать всё наличие":
-        pass
-
-
-
-bot.infinity_polling() 
+bot.infinity_polling()
 
 
 def main():
--- suncatcher/vksunbot.py
+++ suncatcher/vksunbot.py
@@ -1,70 +1,78 @@
 import vk
-import json
 from .config import VK_TOKEN_USER, VK_MARKET_ID
 
-
-def update_info_product():
-    vkapi = vk.API(VK_TOKEN_USER)
-    market_get = vkapi.market.get(count='100', v=5.131, owner_id=VK_MARKET_ID)
-
-    if data == market_get:
-        print('Данные одинаковые. Новых товаров или изменений нет')
-    else:
-        with open("info_product.json", "w") as write_file:
-            json.dump(market_get, write_file)
-            print('Данные разные. Файл с товарами обновлен.')
-
-    # for elem in data['items']:
-    #     print(elem['description'])
-
-    # for elem in data['items']:
-    #     print(elem['thumb_photo'])
-
-    # for elem in data['items']:
-    #     print(elem['price'])
-
-    # for elem in data['items']:
-    #     print(elem['price']['amount'])
-
-    # for elem in data['items']:
-    #     if 'discount_rate' in elem['price']:
-    #         print(elem['price']['discount_rate'])
+vkapi = vk.API(VK_TOKEN_USER)
 
 
-with open("info_product.json", "r") as read_file:
-    data = json.load(read_file)
-#  and ('луна' in elem['title'].lower())
+def market_get():
+    return vkapi.market.get(count='100', extended=1, v=5.131, owner_id=VK_MARKET_ID)
 
 
-def get_big_moon(data, message):
-    description = []
-    title = []
-    url_photo = []
+def get_url_photo_list(data):
+    out_list = []
+    for photo in data['photos']:
+        for x in photo['sizes']:
+            if x['type'] == 'x':
+                out_list.append(x['url'])
+    return out_list
+
+
+def get_moon(size, data=market_get()):
+    suns = {}
+    suns[size] = []
+    suns[f'{size}_count'] = 0
 
     for elem in data['items']:
-        if (elem['title'] != 'Доставка') and (int(elem['price']['amount']) > 310000):
-            description.append(elem['title'] + '\n\n' + elem['description'] + '\n\n' + 'Стоимость - ' + elem['price']['amount'][0:4] + ' ' + elem['price']['currency']['name'])
-            title.append(elem['title'])
-            url_photo.append(elem['thumb_photo'])
+        big = (elem['title'] != 'Доставка') and elem['owner_info']['category'] == 'Большие 40см'
+        mid = (elem['title'] != 'Доставка') and elem['owner_info']['category'] == 'Средние 30см'
+        low = (elem['title'] != 'Доставка') and elem['owner_info']['category'] == 'Малые 20см'
+        suncatcher = {}
 
-    return (description, title, url_photo)
+        suncatcher['id'] = elem['id']
+        suncatcher['title'] = elem['title']
+        suncatcher['description'] = elem['description']
+        suncatcher['price'] = elem['price']['amount']
+        suncatcher['url_photo'] = []
 
-def get_big_lunnica(data, message):
-    description = []
-    title = []
-    url_photo = []
-    id_product = []
-    
+        if big and size == 'big':
+            suns[f'{size}_count'] = suns[f'{size}_count'] + 1
+            suncatcher['url_photo'] = get_url_photo_list(elem)
+            suns['big'].append(suncatcher)
+
+        if mid and size == 'mid':
+            suns[f'{size}_count'] = suns[f'{size}_count'] + 1
+            suncatcher['url_photo'] = get_url_photo_list(elem)
+            suns['mid'].append(suncatcher)
+
+        if low and size == 'low':
+            suns[f'{size}_count'] = suns[f'{size}_count'] + 1
+            suncatcher['url_photo'] = get_url_photo_list(elem)
+            suns['low'].append(suncatcher)
+
+    return suns
+
+
+def get_moon_amount(data=market_get()):
+    amount = {}
+    amount['big'] = 0
+    amount['mid'] = 0
+    amount['low'] = 0
 
     for elem in data['items']:
-        if (elem['title'] != 'Доставка') and (int(elem['price']['amount']) > 310000) and ('лунница' in elem['title'].lower()):
-            description.append(elem['title'] + '\n\n' + elem['description'] + '\n\n' + 'Стоимость - ' + elem['price']['amount'][0:4] + ' ' + elem['price']['currency']['name'])
-            title.append(elem['title'])
-            url_photo.append(elem['thumb_photo'])
-            id_product.append(elem['id'])
+        big = (elem['title'] != 'Доставка') and elem['owner_info']['category'] == 'Большие 40см'
+        mid = (elem['title'] != 'Доставка') and elem['owner_info']['category'] == 'Средние 30см'
+        low = (elem['title'] != 'Доставка') and elem['owner_info']['category'] == 'Малые 20см'
 
+        if big:
+            amount['big'] += 1
 
-    return (description, title, url_photo, id)
+        if mid:
+            amount['mid'] += 1
+
+        if low:
+            amount['low'] += 1
+
+    return amount
 
 
 def main():
